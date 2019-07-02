c7fd625af156b18fdd836d17e8c1c73b95ba3c52
= <=>
2019-07-02 23:27:53 +0800

    LeetCode1025

---
+++ LeetCode/src/leetcode1025/LeetCode1025.java
@@ -0,0 +1,13 @@
+package leetcode1025;
+
+/**
+ * Created by bumblebee on 2019/7/2.
+ */
+public class LeetCode1025 {
+    public boolean divisorGame(int N) {
+        if(N % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+}
