b5bc15aab8344b1b5ed8f343ce04c01b7fd7dd1a
wangsikai@cmcm.com <wangsikai@cmcm.com>
2019-11-04 18:15:59 +0800

    LeetCode2

--- LeetCode/src/leetcode2/LeetCode2.java
+++ LeetCode/src/leetcode2/LeetCode2.java
@@ -24,6 +24,35 @@ public class LeetCode2 {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+
+            int val = x + y + carry;
+            carry = val / 10;
+            cur.next = new ListNode(val % 10);
+            cur = cur.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+
+        return head.next;
+    }
+
+    /*public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int val = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
 
         if (l1 != null) {
@@ -79,7 +108,7 @@ public class LeetCode2 {
         }
 
         return head;
-    }
+    }*/
 
     public static class ListNode {
         int val;
